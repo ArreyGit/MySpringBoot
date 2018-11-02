a9540197e2348704f9bb35e3fa99b5f7e408a965
sunzhirui <Ruijiushian20>
2018-11-02 11:13:02 +0800

    hello world

--- springboot/src/test/java/com/arrey/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/arrey/springboot/SpringbootApplicationTests.java
@@ -2,12 +2,23 @@ package com.arrey.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc //开启mockMvc
 public class SpringbootApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testController() {
+
+    }
 
     @Test
     public void contextLoads() {

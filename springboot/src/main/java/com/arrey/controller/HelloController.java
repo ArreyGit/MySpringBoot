b27b8f68e3a4aae93e09d43fc420762e44d938f1
sunzhirui <Ruijiushian20>
2018-11-02 17:56:32 +0800

    hello world

--- springboot/src/main/java/com/arrey/controller/HelloController.java
+++ springboot/src/main/java/com/arrey/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * @RestController 相当与ResponseBody + Controller
  * GetMapping 相当与@RequestMapping(method = RequestMethod.GET)的缩写
  * 类似的还有@PostMapping、@PutMapping、@DeleteMapping。
  */
